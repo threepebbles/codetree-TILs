b328e1859518d07c850c2fbac214654a9c5d06e1
threepebbles <threepebbles@naver.com>
2024-06-08 16:43:57 +0900

    Title: 자율주행 자동차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240608/자율주행 자동차/autonomous-driving.cpp
+++ 240608/자율주행 자동차/autonomous-driving.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 const int MAXN = 50, MAXM = 50;
 // d : 0123 - 북동남서
-const int dx[] = {-1, 0, 1, 0};
-const int dy[] = {0, 1, 0, -1};
+const int dx[] = { -1, 0, 1, 0 };
+const int dy[] = { 0, 1, 0, -1 };
 int brd[MAXN][MAXM];
 bool visited[MAXN][MAXM];
 
@@ -23,7 +23,7 @@ bool is_out(int x, int y, int n, int m) {
 int main() {
 	int n, m;
 	scanf("%d %d", &n, &m);
-	
+
 	int sx, sy, sd;
 	scanf("%d %d %d", &sx, &sy, &sd);
 	for (int i = 0; i < n; i++) {
@@ -35,19 +35,18 @@ int main() {
 
 	int cx = sx, cy = sy, cd = sd;
 	visited[cx][cy] = true;
+
 	int cnt = 0;
 	while (true) {
 		int nd = left_curve(cd);
 		int nx = cx + dx[nd];
 		int ny = cy + dy[nd];
-		if (!is_out(nx, ny, n, m) && !brd[nx][ny]) {
-			if (!visited[nx][ny]) {
-				cx = nx, cy = ny, cd = nd;
-				visited[cx][cy] = true;
-				
-				cnt = 0;
-				continue;
-			}
+		if (!is_out(nx, ny, n, m) && !brd[nx][ny] && !visited[nx][ny]) {			
+			cx = nx, cy = ny, cd = nd;
+			visited[cx][cy] = true;
+
+			cnt = 0;
+			continue;
 		}
 
 		cd = nd;
@@ -59,6 +58,8 @@ int main() {
 			ny = cy - dy[nd];
 
 			cx = nx, cy = ny, cd = nd;
+			
+			// 후진도 불가한 상황에 탈출
 			if (is_out(cx, cy, n, m) || brd[cx][cy]) break;
 			cnt = 0;
 			continue;
