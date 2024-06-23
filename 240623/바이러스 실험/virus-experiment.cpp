7af9c14cedb7db57f6f813939dd79752afc209f8
threepebbles <threepebbles@naver.com>
2024-06-23 13:50:29 +0900

    Title: 바이러스 실험, Time: 1035ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

--- 240623/바이러스 실험/virus-experiment.cpp
+++ 240623/바이러스 실험/virus-experiment.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
 struct Virus {
@@ -10,44 +11,65 @@ struct Virus {
 	Virus(int _r, int _c, int _age, bool _alive) : r(_r), c(_c), age(_age), alive(_alive) {}
 };
 
+struct cmp {
+	bool operator() (Virus a, Virus b) {
+		return a.age > b.age;
+	}
+};
+
 const int MAXN = 10;
-const int dr[] = {-1, -1, -1, 0, 1, 1, 1, 0};
-const int dc[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+const int dr[] = { -1, -1, -1, 0, 1, 1, 1, 0 };
+const int dc[] = { -1, 0, 1, 1, 1, 0, -1, -1 };
 
 int n, m, k;
 int brd[MAXN][MAXN];
 int add_amount[MAXN][MAXN];	// 추가되는 양분의 양
 
-vector<Virus> do_cycle(vector<Virus>& vs) {
-	vector<Virus> new_vs;
+priority_queue<Virus, vector<Virus>, cmp> do_cycle(priority_queue<Virus, vector<Virus>, cmp>& vs) {
+	queue<Virus> new_vs;
 
 	// age순 오름차순 정렬
-	sort(vs.begin(), vs.end(),
+	/*sort(vs.begin(), vs.end(),
 		[](const Virus& x, const Virus& y) -> bool {
 			return x.age < y.age;
-		});
-	
+		});*/
+
+	queue<Virus> dead;
 	// 양분 섭취
-	for (Virus& v : vs) {
+	while(!vs.empty()) {
+		Virus v = vs.top();
+		vs.pop();
+
 		if (brd[v.r][v.c] >= v.age) {
 			brd[v.r][v.c] -= v.age;
 			v.age++;
-			new_vs.push_back(v);
+			new_vs.push(v);
 		}
 		else {
 			// 아사
 			v.alive = false;
+			dead.push(v);
 		}
 	}
-	
+
 	// 죽은 바이러스는 양분으로
-	for (Virus& v : vs) {
+	
+	while (!dead.empty()) {
+		Virus v = dead.front();
+		dead.pop();
+
 		if (v.alive) continue;
 		brd[v.r][v.c] += v.age / 2;
 	}
 
+	priority_queue<Virus, vector<Virus>, cmp> ret;
 	// 번식 진행
-	for (Virus& v : vs) {
+	while (!new_vs.empty()) {
+		Virus v = new_vs.front();
+		new_vs.pop();
+
+		ret.push(v);
+
 		if (!v.alive) continue;
 		if (v.age % 5 != 0) continue;
 
@@ -56,7 +78,7 @@ vector<Virus> do_cycle(vector<Virus>& vs) {
 			int nr = v.r + dr[d];
 			int nc = v.c + dc[d];
 			if (nr < 0 || nr >= n || nc < 0 || nc >= n) continue;
-			new_vs.push_back(Virus(nr, nc, 1, true));
+			ret.push(Virus(nr, nc, 1, true));
 		}
 	}
 
@@ -66,7 +88,7 @@ vector<Virus> do_cycle(vector<Virus>& vs) {
 			brd[i][j] += add_amount[i][j];
 		}
 	}
-	return new_vs;
+	return ret;
 }
 
 int main() {
@@ -78,13 +100,13 @@ int main() {
 		}
 	}
 
-	vector<Virus> vs;
+	priority_queue<Virus, vector<Virus>, cmp> vs;
 	for (int i = 0, r, c, age; i < m; i++) {
 		scanf("%d %d %d", &r, &c, &age);
 		r--, c--;
-		vs.push_back(Virus(r, c, age, true));
+		vs.push(Virus(r, c, age, true));
 	}
-	
+
 	while (k--) {
 		vs = do_cycle(vs);
 	}
