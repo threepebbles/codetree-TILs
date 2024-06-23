c1f08e4344846db62642aff58fba997a433aac38
threepebbles <threepebbles@naver.com>
2024-06-23 10:10:13 +0900

    Title: 바이러스 실험, Time: 629ms, Memory: 1MB, Status: Passed - CodeTree

--- 240623/바이러스 실험/virus-experiment.cpp
+++ 240623/바이러스 실험/virus-experiment.cpp
@@ -6,7 +6,8 @@ using namespace std;
 struct Virus {
 	int r, c;
 	int age;
-	Virus(int _r, int _c, int _age) : r(_r), c(_c), age(_age) {}
+	bool alive;
+	Virus(int _r, int _c, int _age, bool _alive) : r(_r), c(_c), age(_age), alive(_alive) {}
 };
 
 const int MAXN = 10;
@@ -25,7 +26,7 @@ vector<Virus> do_cycle(vector<Virus>& vs) {
 		[](const Virus& x, const Virus& y) -> bool {
 			return x.age < y.age;
 		});
-
+	
 	// 양분 섭취
 	for (Virus& v : vs) {
 		if (brd[v.r][v.c] >= v.age) {
@@ -33,20 +34,29 @@ vector<Virus> do_cycle(vector<Virus>& vs) {
 			v.age++;
 			new_vs.push_back(v);
 		}
-		// die
 		else {
-			// 본인이 있던 칸에 나이/2 만큼 양분 추가
-			brd[v.r][v.c] += v.age / 2;
-			continue;
+			// 아사
+			v.alive = false;
 		}
-		
+	}
+	
+	// 죽은 바이러스는 양분으로
+	for (Virus& v : vs) {
+		if (v.alive) continue;
+		brd[v.r][v.c] += v.age / 2;
+	}
+
+	// 번식 진행
+	for (Virus& v : vs) {
+		if (!v.alive) continue;
 		if (v.age % 5 != 0) continue;
+
 		// 살아있는 바이러스 중에 나이가 5의 배수인 경우, 번식
 		for (int d = 0; d < 8; d++) {
 			int nr = v.r + dr[d];
 			int nc = v.c + dc[d];
 			if (nr < 0 || nr >= n || nc < 0 || nc >= n) continue;
-			new_vs.push_back(Virus(nr, nc, 1));
+			new_vs.push_back(Virus(nr, nc, 1, true));
 		}
 	}
 
@@ -72,7 +82,7 @@ int main() {
 	for (int i = 0, r, c, age; i < m; i++) {
 		scanf("%d %d %d", &r, &c, &age);
 		r--, c--;
-		vs.push_back(Virus(r, c, age));
+		vs.push_back(Virus(r, c, age, true));
 	}
 	
 	while (k--) {
