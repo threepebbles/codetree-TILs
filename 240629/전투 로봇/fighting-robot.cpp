3b9cc313e587dacaa569e74417fae58ae33f5278
threepebbles <threepebbles@naver.com>
2024-06-29 16:24:38 +0900

    Title: 전투 로봇, Time: 16ms, Memory: 0MB, Status: Passed - CodeTree

--- 240629/전투 로봇/fighting-robot.cpp
+++ 240629/전투 로봇/fighting-robot.cpp
@@ -4,15 +4,20 @@
 #include <memory.h>
 using namespace std;
 
-const int MAXN = 20;
-const int INF = 1e9;
-const int dr[] = { -1, 0, 1, 0 };
-const int dc[] = { 0, -1, 0, 1 };
-
 struct Crd {
 	int r, c;
 	Crd() {}
 	Crd(int _r, int _c) : r(_r), c(_c) {}
+};
+
+struct Robot {
+	Crd crd;
+	int level;
+	int cnt;
+	Robot() {}
+	Robot(Crd _crd, int _level) : crd(_crd), level(_level) {
+		cnt = 0;
+	}
 };
 
 struct V {
@@ -22,15 +27,10 @@ struct V {
 	V(int _r, int _c, int _dis) : r(_r), c(_c), dis(_dis) {}
 };
 
-struct Robot {
-	Crd crd;
-	int level;
-	int cnt;
-	Robot() {}
-	Robot(Crd _crd, int _level) : crd(_crd), level(_level) {
-		cnt = 0; 
-	}
-};
+const int MAXN = 20;
+const int INF = 1e9;
+const int dr[] = { -1, 0, 1, 0 };
+const int dc[] = { 0, -1, 0, 1 };
 
 int n;
 int brd[MAXN][MAXN];
@@ -38,9 +38,10 @@ bool visited[MAXN][MAXN];
 
 Crd choose_crd_by_rule(Crd crd_monster1, Crd crd_monster2) {
 	Crd list_monster[] = { crd_monster1, crd_monster2 };
+	// 우선순위: 가장 위쪽, 가장 왼쪽 순
 	sort(list_monster, list_monster + 2, [](const Crd& c1, const Crd& c2) -> bool {
 		return c1.r == c2.r ? c1.c < c2.c : c1.r < c2.r;
-	});
+		});
 	return list_monster[0];
 }
 
@@ -67,7 +68,7 @@ V find_closest_monster(Robot& robot) {
 
 			Crd crd_next = Crd(nr, nc);
 			int dis_next = cur.dis + 1;
-			que.push(V(nr, nc, cur.dis+1));
+			que.push(V(nr, nc, cur.dis + 1));
 
 			if (brd[nr][nc] != 0 && brd[nr][nc] < robot.level) {
 				if (dis_next < dis_min) {
@@ -86,21 +87,27 @@ V find_closest_monster(Robot& robot) {
 
 int solve(Robot& robot) {
 	int ret = 0;
-	
+
 	while (true) {
 		V monster = find_closest_monster(robot);
+		// 없앨 수 있는 몬스터 없음
 		if (monster.r == -1) break;
 
-		ret += monster.dis;
-		brd[monster.r][monster.c] = 0;
+		// 몬스터 위치로 이동
 		robot.crd = Crd(monster.r, monster.c);
+		ret += monster.dis;
+
+		// 몬스터 제거
+		brd[monster.r][monster.c] = 0;
+		
+		// 경험치 상승
 		robot.cnt++;
 		if (robot.cnt == robot.level) {
 			robot.level++;
 			robot.cnt = 0;
 		}
 	}
-	
+
 	return ret;
 }
 
