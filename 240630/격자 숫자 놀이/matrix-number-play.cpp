58eb347711bb1a92d5eef33f98dfa7979aaff2c8
threepebbles <threepebbles@naver.com>
2024-06-30 10:49:53 +0900

    Title: 격자 숫자 놀이, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240630/격자 숫자 놀이/matrix-number-play.cpp
+++ 240630/격자 숫자 놀이/matrix-number-play.cpp
@@ -1,3 +1,7 @@
+/*
+* 격자 숫자 놀이
+* 시간복잡도: O(n^2*log(n))
+*/
 #include <cstdio>
 #include <algorithm>
 #include <vector>
@@ -11,11 +15,12 @@ struct V {
 };
 
 const int MAXN = 200;
+const int MAXK = 100;
 
 int r_target, c_target, k_target;
 int brd[MAXN][MAXN];
 int new_brd[MAXN][MAXN];
-int cnt[101];
+int cnt[MAXK+1];
 
 int main() {
 	scanf("%d %d %d", &r_target, &c_target, &k_target);
@@ -28,8 +33,9 @@ int main() {
 	}
 
 	int n = 3, m = 3;
-
 	int ans = -1;
+	// 시간복잡도: O(100 * n * (n + 100 + n*log(n) + n)
+	// = O(n * n*log(n))
 	for (int t = 0; t < 100; t++) {
 		if (brd[r_target][c_target] == k_target) {
 			ans = t;
@@ -40,25 +46,31 @@ int main() {
 		if (n >= m) {
 			int m_max = -1;
 			for (int r = 0; r < n; r++) {
+				// 빈도수 계산
 				vector<V> freqs;
 				for (int c = 0; c < m; c++) {
 					cnt[brd[r][c]]++;
 				}
-				for (int i = 1; i <= 100; i++) {
-					if (!cnt[i]) continue;
-					freqs.push_back(V(i, cnt[i]));
+				for (int num = 1; num <= MAXK; num++) {
+					if (!cnt[num]) continue;
+					freqs.push_back(V(num, cnt[num]));
 
 					// initialize
-					cnt[i] = 0;
+					cnt[num] = 0;
 				}
+
+				// 빈도수(cnt) 오름차순 정렬, 빈도수가 같다면 숫자(num) 기준 오름차순으로 정렬
 				sort(freqs.begin(), freqs.end(), 
 					[](const V& v1, const V& v2) {
 						if (v1.cnt == v2.cnt) return v1.num < v2.num;
 						return v1.cnt < v2.cnt;
 					}
 				);
+
+				// 열 크기 갱신
 				m_max = max(m_max, (int)freqs.size() * 2);
 				
+				// 새로운 배열 갱신
 				int c = 0;
 				for (V& freq : freqs) {
 					new_brd[r][c++] = freq.num;
@@ -76,12 +88,12 @@ int main() {
 					cnt[brd[r][c]]++;
 				}
 
-				for (int i = 1; i <= 100; i++) {
-					if (!cnt[i]) continue;
-					freqs.push_back(V(i, cnt[i]));
-					
+				for (int num = 1; num <= MAXK; num++) {
+					if (!cnt[num]) continue;
+					freqs.push_back(V(num, cnt[num]));
+
 					// initialize
-					cnt[i] = 0;
+					cnt[num] = 0;
 				}
 
 				sort(freqs.begin(), freqs.end(),
