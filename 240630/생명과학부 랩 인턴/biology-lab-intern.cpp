64ec4e20fd98e460ec5bb7e53ad821998bb625a3
threepebbles <threepebbles@naver.com>
2024-06-30 10:54:37 +0900

    Title: 생명과학부 랩 인턴, Time: 17ms, Memory: 0MB, Status: Passed - CodeTree

--- 240630/생명과학부 랩 인턴/biology-lab-intern.cpp
+++ 240630/생명과학부 랩 인턴/biology-lab-intern.cpp
@@ -1,3 +1,7 @@
+/*
+* 생명과학부 랩 인턴
+* 시간복잡도: O(n^2*m)
+*/
 #include <cstdio>
 #include <vector>
 #include <algorithm>
@@ -8,19 +12,19 @@ struct V {
 	int r, c;
 	int s, d, b;
 	V() { s = -1, d = -1, b = 0; }
-	V(int _r, int _c, int _s, int _d, int _b): r(_r), c(_c), s(_s), d(_d), b(_b) {}
+	V(int _r, int _c, int _s, int _d, int _b) : r(_r), c(_c), s(_s), d(_d), b(_b) {}
 };
 
 const int MAXN = 100, MAXM = 100;
 // 위, 아래, 오른쪽, 왼쪽 순서
-const int dr[] = {-1, 1, 0, 0};
-const int dc[] = {0, 0, 1, -1};
+const int dr[] = { -1, 1, 0, 0 };
+const int dc[] = { 0, 0, 1, -1 };
 
 int n, m, k;
 V brd[MAXN][MAXM];
 V new_brd[MAXN][MAXM] = {};
 
-// O(m*log(m))
+// O(n)
 V find_pange_and_erase(int col) {
 	V ret = V();
 	for (int r = 0; r < n; r++) {
@@ -30,7 +34,7 @@ V find_pange_and_erase(int col) {
 			break;
 		}
 	}
-	return ret;;
+	return ret;
 }
 
 int reverse_direction(int d) {
@@ -40,19 +44,21 @@ int reverse_direction(int d) {
 	return 2;
 }
 
+// 크기가 큰 곰팡이가 잡아먹음
 V choose(V v1, V v2) {
 	return v1.b > v2.b ? v1 : v2;
 }
 
-
+// O(n*m)
 void move_for_second() {
 	memset(new_brd, 0, sizeof(new_brd));
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if(brd[i][j].b > 0) {
+			if (brd[i][j].b > 0) {
 				V x = brd[i][j];
-				int nr = x.r + dr[x.d]*x.s;
+				int nr = x.r + dr[x.d] * x.s;
 				int nc = x.c + dc[x.d] * x.s;
+
 				// 상하
 				if (x.d <= 1) {
 					nr += 2 * (n - 1);
@@ -86,6 +92,7 @@ void move_for_second() {
 			}
 		}
 	}
+
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			brd[i][j] = new_brd[i][j];
@@ -100,28 +107,27 @@ int main() {
 		// 0베이스
 		x--, y--;
 		d--;
+
 		// 상하
 		if (d <= 1) {
-			s %= 2 * (n-1);
+			s %= 2 * (n - 1);
 		}
 		// 좌우
 		else {
-			s %= 2 * (m-1);
+			s %= 2 * (m - 1);
 		}
 		// s: 초속, d: 이동방향, b: 곰팡이 크기
 		brd[x][y] = V(x, y, s, d, b);
 	}
 
 	int sum = 0;
-	// 채취 시작
+	// 열 순서대로 순회
 	for (int col = 0; col < m; col++) {
-		
-		// 2. 곰팡이 채취
+		// 곰팡이 채취
 		V target = find_pange_and_erase(col);
 		sum += target.b;
-		
+
 		// 곰팡이 이동
-		// 같은 칸에 있는 경우 큰 곰팡이가 잡아먹음
 		move_for_second();
 	}
 	printf("%d", sum);
