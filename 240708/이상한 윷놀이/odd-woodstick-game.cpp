3ae7ab2014bf6d8c0395836a07f9de845134a90c
threepebbles <threepebbles@naver.com>
2024-07-08 10:03:21 +0900

    Title: 이상한 윷놀이, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240708/이상한 윷놀이/odd-woodstick-game.cpp
+++ 240708/이상한 윷놀이/odd-woodstick-game.cpp
@@ -1,123 +1,244 @@
 #include <cstdio>
 #include <vector>
+#include <algorithm>
+#include <stdlib.h>
 using namespace std;
 
-struct H {
+struct V {
 	int x, y;
 	int d;
-	int floor;
-	H() {}
-	H(int _x, int _y, int _d, int _f) :x(_x), y(_y), d(_d), floor(_f) {}
+	V* upper = NULL;
+
+	V() {}
+	V(int _x, int _y, int _d) :x(_x), y(_y), d(_d) { }
 };
 
-const int MAXN = 12;
+const int MAXN = 12, MAXK = 10;
+// 격자 색깔
+const int WHITE = 0;
+const int RED = 1;
+const int BLUE = 2;
+
 const int MAX_TURN = 1000;
 
-const int RIGHT = 1, LEFT = 2, UP = 3, DOWN = 4;
-const int WHITE = 0, RED = 1, BLUE = 2;
+// 방향
+const int RIGHT = 1;
+const int LEFT = 2;
+const int UP = 3;
+const int DOWN = 4;
+
+// 위, 오른쪽, 아래, 왼쪽 순서
+const int dx[] = { -1, 0, 1, 0 };
+const int dy[] = { 0, 1, 0, -1 };
 
 int n, k;
 int brd[MAXN][MAXN];
-vector<int> state[MAXN][MAXN];
-vector<H> horses;
-
-// 상 우 하 좌 순서
-int dx[] = { -1, 0, 1, 0 };
-int dy[] = { 0, 1, 0, -1 };
-
-void update_crd(int x, int y) {
-	for (int& hidx: state[x][y]) {
-		horses[hidx].x = x;
-		horses[hidx].y = y;
-	}
-}
+V* state[MAXN][MAXN];	// i, j에 놓여있는 가장 아래쪽 말 저장
+vector<V*> horses;
 
 bool is_in_range(int x, int y) {
 	if (x < 0 || x >= n || y < 0 || y >= n) return false;
 	return true;
 }
 
-bool do_move(int hidx) {
-	H& h = horses[hidx];
-	int nx = h.x + dx[h.d];
-	int ny = h.y + dy[h.d];
-	int nf = 0;
+V* find_upper_horse(V* start) {
+	V* cur = start;
+	return cur->upper;
+}
+
+V* find_lower_horse(V* bottom, V* target) {
+	V* cur = bottom;
+	V* nxt = cur->upper;
+
+	while (nxt != target) {
+		cur = nxt;
+		nxt = cur->upper;
+	}
+	return cur;
+}
+
+V* find_top_horse(V* start) {
+	V* cur = start;
+	V* nxt = cur->upper;
+	while (nxt != NULL) {
+		cur = nxt;
+		nxt = cur->upper;
+	}
+	return cur;
+}
+
+void update_crd(V* bottom) {
+	V* cur = bottom;
+	if (cur == NULL) return;
+
+	int x = bottom->x;
+	int y = bottom->y;
+
+	while (cur != NULL) {
+		cur->x = x;
+		cur->y = y;
+		cur = cur->upper;
+	}
+}
+
+int count_horse(V* bottom) {
+	int cnt = 0;
+	V* cur = bottom;
+	while (cur != NULL) {
+		cnt++;
+		cur = cur->upper;
+	}
+	return cnt;
+}
+
+bool do_white(V* h) {
+	// 움직이기 전, 맨 아래 위치였던 경우
+	if (state[h->x][h->y] == h) {
+		state[h->x][h->y] = NULL;
+	}
+	// 맨 아래가 아니었던 경우
+	else {
+		V* lower_horse = find_lower_horse(state[h->x][h->y], h);
+		lower_horse->upper = NULL;
+	}
+
+	int nx = h->x + dx[h->d];
+	int ny = h->y + dy[h->d];
+
+	h->x = nx;
+	h->y = ny;
+
+	// 이동
+	// 빈 자리면
+	if (state[h->x][h->y] == NULL) {
+		state[h->x][h->y] = h;
+	}
+	// 말이 이미 있으면
+	else {
+		V* top_horse = find_top_horse(state[h->x][h->y]);
+		top_horse->upper = h;
+	}
+	update_crd(state[h->x][h->y]);
+	return count_horse(state[h->x][h->y]) >= 4;
+}
+
+bool do_red(V* h) {
+	// 움직이기 전, 맨 아래 위치였던 경우
+	if (state[h->x][h->y] == h) {
+		state[h->x][h->y] = NULL;
+	}
+	// 맨 아래가 아니었던 경우
+	else {
+		V* lower_horse = find_lower_horse(state[h->x][h->y], h);
+		lower_horse->upper = NULL;
+	}
+
+	int nx = h->x + dx[h->d];
+	int ny = h->y + dy[h->d];
+
+	h->x = nx;
+	h->y = ny;
+
+	// 순서 뒤집기
+	vector<V*> order;
+	V* cur = h;
+	while (cur != NULL) {
+		cur->x = nx;
+		cur->y = ny;
+		order.push_back(cur);
+		V* nxt = cur->upper;
+		// 원래 방향 링크 삭제
+		cur->upper = NULL;
+
+		cur = nxt;
+	}
+
+	// 순서 거꾸로 링크
+	reverse(order.begin(), order.end());
+	cur = order[0];
+	for (int i = 1; i < order.size(); i++) {
+		cur->upper = order[i];
+		cur = order[i];
+	}
+
+	// 이동
+	// 비어있으면
+	if (state[h->x][h->y] == NULL) {
+		state[h->x][h->y] = order[0];
+	}
+	// 이미 말이 있으면
+	else {
+		V* top_horse = find_top_horse(state[h->x][h->y]);
+		top_horse->upper = order[0];
+	}
+	update_crd(state[h->x][h->y]);
+	return count_horse(state[h->x][h->y]) >= 4;
+}
+
+bool do_move(V* h) {
+	int nx = h->x + dx[h->d];
+	int ny = h->y + dy[h->d];
 
 	if (!is_in_range(nx, ny) || brd[nx][ny] == BLUE) {
-		// 방향 전환
-		h.d = (h.d + 2) % 4;
+		h->d = (h->d + 2) % 4;
+		nx = h->x + dx[h->d];
+		ny = h->y + dy[h->d];
 
-		nx = h.x + dx[h.d];
-		ny = h.y + dy[h.d];
-
-		// 반대방향도 막힌 경우, 이동 종료
 		if (!is_in_range(nx, ny) || brd[nx][ny] == BLUE) {
 			return false;
 		}
 	}
-	
+
 	if (brd[nx][ny] == WHITE) {
-		// 얹혀 있는 말 포함 이동
-		int cur_f = h.floor;
-		int sz = state[h.x][h.y].size();
-		for (int f = cur_f; f < sz; f++) {
-			horses[state[h.x][h.y][f]].floor = state[nx][ny].size();
-			state[nx][ny].push_back(state[h.x][h.y][f]);
-		}
-		for (int f = cur_f; f < sz; f++) {
-			state[h.x][h.y].pop_back();
-		}
+		return do_white(h);
 	}
 	else if (brd[nx][ny] == RED) {
-		// 얹혀 있는 말 포함 이동
-		int cur_f = h.floor;
-		int sz = state[h.x][h.y].size();
-		for (int f = sz-1; f >= cur_f; f--) {
-			horses[state[h.x][h.y][f]].floor = state[nx][ny].size();
-			state[nx][ny].push_back(state[h.x][h.y][f]);
-		}
-		for (int f = cur_f; f < sz; f++) {
-			state[h.x][h.y].pop_back();
-		}
+		return do_red(h);
 	}
-	update_crd(nx, ny);
 
-	if (!(h.x == nx && h.y == ny)) {
-		printf("not same\n");
-
-	}
-	return state[h.x][h.y].size() >= 4;
+	// 도달할 일 없음
+	return false;
 }
 
 int main() {
 	scanf("%d %d", &n, &k);
-
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			scanf("%d", &brd[i][j]);
 		}
 	}
+
 	for (int i = 0, x, y, d; i < k; i++) {
 		scanf("%d %d %d", &x, &y, &d);
+		// 0 베이스
 		x--, y--;
+
 		if (d == RIGHT) d = 1;
 		else if (d == LEFT) d = 3;
 		else if (d == UP) d = 0;
-		else if (d == DOWN) d = 2;
+		else d = 2;
 
-		horses.push_back(H(x, y, d, 0));
-		state[x][y].push_back(horses.size() - 1);
+		V* h = (V*)malloc(sizeof(struct V));
+		h->x = x;
+		h->y = y;
+		h->d = d;
+		h->upper = NULL;
+		// 같은 칸에 두 개의 말이 주어지는 경우는 없음
+		horses.push_back(h);
+		state[x][y] = h;
 	}
+
 
 	int turn = 0;
 	bool is_end = false;
 	while (!is_end && turn < MAX_TURN) {
 		turn++;
 
-		// 이동
-		for (int i = 0; i < horses.size() && !is_end; i++) {
-			is_end |= do_move(i);
+		// 순서대로 말 이동
+		for (int hi = 0; hi < horses.size() && !is_end; hi++) {
+			is_end |= do_move(horses[hi]);
 		}
 	}
-	printf("%d", turn >= MAX_TURN ? -1 : turn);
+
+	printf("%d", turn == MAX_TURN ? -1 : turn);
 }
