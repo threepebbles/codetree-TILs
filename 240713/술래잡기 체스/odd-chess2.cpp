a22fcd93df3aa622d2f1ac34dde0749c87466e07
threepebbles <threepebbles@naver.com>
2024-07-13 16:22:47 +0900

    Title: 술래잡기 체스, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240713/술래잡기 체스/odd-chess2.cpp
+++ 240713/술래잡기 체스/odd-chess2.cpp
@@ -20,7 +20,18 @@ bool is_in_range(int r, int c) {
 	return true;
 }
 
-void move_thieves(vector<vector<int>>& brd, vector<V>& ts) {
+int find_thief(int r, int c, vector<V>& ts) {
+	for (int i = 1; i <= ROW_SIZE * COL_SIZE; i++) {
+		V t = ts[i];
+		if (t.d == -1) continue;
+
+		if (t.r == r && t.c == c) return i;
+	}
+	return 0;
+}
+
+void move_thieves(V& chaser, vector<V>& ts) {
+	vector<V> new_ts = ts;
 	for (int i = 1; i <= 16; i++) {
 		// 잡힌 도둑 패스
 		if (ts[i].d == -1) continue;
@@ -29,7 +40,9 @@ void move_thieves(vector<vector<int>>& brd, vector<V>& ts) {
 		while (true) {
 			int nr = ts[i].r + dr[ts[i].d];
 			int nc = ts[i].c + dc[ts[i].d];
-			if (!is_in_range(nr, nc) || brd[nr][nc] == NUM_CHASER) {
+			// 격자 밖이거나, 술래가 있는 경우
+			if (!is_in_range(nr, nc) 
+				|| (ts[i].r==chaser.r && ts[i].c==chaser.c)) {
 				ts[i].d++;
 
 				// 이동 불가
@@ -40,36 +53,27 @@ void move_thieves(vector<vector<int>>& brd, vector<V>& ts) {
 			}
 			
 			// 이동
-			// 빈칸
-			if (brd[nr][nc] == 0) {
-				brd[nr][nc] = i;
-				brd[ts[i].r][ts[i].c] = 0;
+			int num_target = find_thief(nr, nc, ts);
 
+			//빈칸
+			if (num_target == 0) {
 				ts[i].r = nr;
 				ts[i].c = nc;
 			}
-			// 다른 도둑과 swap
 			else {
-				int num_target = brd[nr][nc];
-				
-				brd[ts[i].r][ts[i].c] = num_target;
-				brd[nr][nc] = i;
-
 				ts[num_target].r = ts[i].r;
 				ts[num_target].c = ts[i].c;
-
 				ts[i].r = nr;
 				ts[i].c = nc;
 			}
-			
 			break;
 		}
 	}
 }
 
 int ans;
-void dfs(V& chaser, vector<vector<int>>& brd, vector<V>& ts, int score) {
-	move_thieves(brd, ts);
+void dfs(V& chaser, vector<V>& ts, int score) {
+	move_thieves(chaser, ts);
 
 	vector<int> cands;
 	int r = chaser.r, c = chaser.c, d = chaser.d;
@@ -79,7 +83,7 @@ void dfs(V& chaser, vector<vector<int>>& brd, vector<V>& ts, int score) {
 		if (!is_in_range(nr, nc)) break;
 		
 		r = nr, c = nc;
-		int num_thief = brd[r][c];
+		int num_thief = find_thief(r, c, ts);
 		if (num_thief != 0) {
 			cands.push_back(num_thief);
 		}
@@ -90,28 +94,18 @@ void dfs(V& chaser, vector<vector<int>>& brd, vector<V>& ts, int score) {
 		return;
 	}
 
-	vector<vector<int>> brd_tmp = brd;
-	vector<V> ts_tmp = ts;
 	for (int num_thief: cands) {
 		V cand = ts[num_thief];
 		V chaser_next = V(cand.r, cand.c, ts[num_thief].d);
+		vector<V> ts_next = ts;
+		ts_next[num_thief] = V(-1, -1, -1);
 		int score_next = score + num_thief;
 
-		brd[chaser.r][chaser.c] = 0;
-		brd[cand.r][cand.c] = NUM_CHASER;
-		ts[num_thief] = V(-1, -1, -1);
-
-		dfs(chaser_next, brd, ts, score + num_thief);
-		// 복원
-		brd = brd_tmp;
-		ts = ts_tmp;
+		dfs(chaser_next, ts_next, score_next);
 	}
 }
 
 int main() {
-	// board. 0이면 빈칸
-	vector<vector<int>> brd(ROW_SIZE, vector<int>(COL_SIZE, 0));
-
 	V chaser;
 	// d==-1 이면 사망
 	vector<V> thieves(ROW_SIZE*COL_SIZE+1, V(-1, -1, -1));
@@ -121,18 +115,16 @@ int main() {
 			scanf("%d %d", &num, &d);
 			d--;
 			thieves[num] = V(i, j, d);
-			brd[i][j] = num;
 		}
 	}
 	// (0, 0)에 있는 도둑말 잡기
 	int score = 0;
-	int num_theif = brd[0][0];
+	int num_theif = find_thief(0, 0, thieves);
 	
 	chaser = V(0, 0, thieves[num_theif].d);
 	score += num_theif;
 	thieves[num_theif] = V(-1, -1, -1);
-	brd[0][0] = NUM_CHASER;
 
-	dfs(chaser, brd, thieves, score);
+	dfs(chaser, thieves, score);
 	printf("%d", ans);
 }
