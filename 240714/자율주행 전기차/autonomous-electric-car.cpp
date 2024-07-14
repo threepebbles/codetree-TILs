8c75849fdcc985d4653d2a2fcb950a86918c1084
threepebbles <threepebbles@naver.com>
2024-07-14 12:15:42 +0900

    Title: 자율주행 전기차, Time: 55ms, Memory: 0MB, Status: Passed - CodeTree

--- 240714/자율주행 전기차/autonomous-electric-car.cpp
+++ 240714/자율주행 전기차/autonomous-electric-car.cpp
@@ -7,18 +7,18 @@ using namespace std;
 typedef long long ll;
 struct Car {
 	int r, c;
-	ll b;
+	ll battery;
 	Car() {}
-	Car(int _r, int _c, ll _b) :r(_r), c(_c), b(_b) {}
+	Car(int _r, int _c, ll _b): r(_r), c(_c), battery(_b) {}
 };
 
-struct C {
+struct Customer {
 	int r_st, c_st;
 	int r_ed, c_ed;
 	int num;
 	bool is_arrive;
-	C() {};
-	C(int _rst, int _cst, int _red, int _ced, int _num, bool _f) : r_st(_rst), c_st(_cst), r_ed(_red), c_ed(_ced), num(_num), is_arrive(_f) {}
+	Customer() {};
+	Customer(int _rst, int _cst, int _red, int _ced, int _num, bool _f) : r_st(_rst), c_st(_cst), r_ed(_red), c_ed(_ced), num(_num), is_arrive(_f) {}
 };
 
 struct V {
@@ -32,10 +32,10 @@ const int WALL = 1;
 const int dr[] = { -1, 1, 0, 0 };
 const int dc[] = { 0, 0, -1, 1 };
 
-int n, m, c;
+int n, m;
 int brd[MAXN][MAXN];
 Car car;
-C customers[MAXN * MAXN];
+Customer customers[MAXN * MAXN + 1];
 
 bool is_in_range(int r, int c) {
 	if (r < 0 || r >= n || c < 0 || c >= n) return false;
@@ -48,7 +48,7 @@ bool visited[MAXN][MAXN] = {};
 int bfs(int r_st, int c_st, int r_target, int c_target) {
 	queue<V> que;
 	que.push(V(r_st, c_st));
-	
+
 	memset(visited, false, sizeof(visited));
 	memset(dis, -1, sizeof(dis));
 	dis[r_st][c_st] = 0;
@@ -85,7 +85,7 @@ int find_customer() {
 	while (!que.empty()) {
 		V cur = que.front();
 		que.pop();
-		
+
 		if (visited[cur.r][cur.c]) continue;
 		visited[cur.r][cur.c] = true;
 
@@ -101,18 +101,20 @@ int find_customer() {
 		}
 	}
 
-	vector<C> cands;
+	vector<Customer> cands;
 	for (int i = 1; i <= m; i++) {
-		C c = customers[i];
+		Customer c = customers[i];
+		if (dis[c.r_st][c.c_st] == -1) continue;
 		if (c.is_arrive) continue;
+
 		cands.push_back(c);
 	}
 	if (cands.size() == 0) {
 		return -1;
 	}
 
-	sort(cands.begin(), cands.end(), 
-		[&](C& c1, C& c2) {
+	sort(cands.begin(), cands.end(),
+		[&](Customer& c1, Customer& c2) {
 			if (dis[c1.r_st][c1.c_st] == dis[c2.r_st][c2.c_st]) {
 				if (c1.r_st == c2.r_st) return c1.c_st < c2.c_st;
 				else return c1.r_st < c2.r_st;
@@ -120,15 +122,15 @@ int find_customer() {
 			else {
 				return dis[c1.r_st][c1.c_st] < dis[c2.r_st][c2.c_st];
 			}
-			
+
 		});
 
 	return cands[0].num;
 }
 
 int main() {
-
-	scanf("%d %d %d", &n, &m, &c);
+	ll c;
+	scanf("%d %d %lld", &n, &m, &c);
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			scanf("%d", &brd[i][j]);
@@ -138,31 +140,35 @@ int main() {
 	int r_car, c_car;
 	scanf("%d %d", &r_car, &c_car);
 	r_car--, c_car--;
-	car = Car(r_car, c_car, (ll)c);
+	car = Car(r_car, c_car, c);
 
 	for (int i = 1; i <= m; i++) {
 		int r_st, c_st, r_ed, c_ed;
 		scanf("%d %d %d %d", &r_st, &c_st, &r_ed, &c_ed);
 		r_st--, c_st--, r_ed--, c_ed--;
-		customers[i] = C(r_st, c_st, r_ed, c_ed, i, false);
+		customers[i] = Customer(r_st, c_st, r_ed, c_ed, i, false);
 	}
 
 	// 다음 고객 선택
 	for (int i = 0; i < m; i++) {
 		int c_next = find_customer();
-		C& c = customers[c_next];
+		if (c_next == -1) {
+			puts("-1");
+			return 0;
+		}
+		Customer& c = customers[c_next];
 		int dis1 = bfs(car.r, car.c, c.r_st, c.c_st);
 		int dis2 = bfs(c.r_st, c.c_st, c.r_ed, c.c_ed);
-		
+
 		// 이동 불가
 		if (dis1 == -1 || dis2 == -1) {
 			puts("-1");
 			return 0;
 		}
 
-		if (dis1 + dis2 <= car.b) {
-			car.b -= (dis1 + dis2);
-			car.b += dis2 * 2ll;
+		if (dis1 + dis2 <= car.battery) {
+			car.battery -= (dis1 + dis2);
+			car.battery += dis2 * 2ll;
 			car.r = c.r_ed;
 			car.c = c.c_ed;
 			c.is_arrive = true;
@@ -174,5 +180,5 @@ int main() {
 		}
 	}
 
-	printf("%lld", car.b);
+	printf("%lld", car.battery);
 }
