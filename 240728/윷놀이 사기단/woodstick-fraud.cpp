5dc4b36f0c82204fdda242302dc14f7aa81c655f
threepebbles <threepebbles@naver.com>
2024-07-28 05:30:56 +0900

    Title: 윷놀이 사기단, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

--- 240728/윷놀이 사기단/woodstick-fraud.cpp
+++ 240728/윷놀이 사기단/woodstick-fraud.cpp
@@ -10,6 +10,26 @@ int move_cnt[10];
 vector<int> g[NODE_SIZE];
 int score[NODE_SIZE];
 
+void build_score() {
+	for (int i = 1; i <= 20; i++) {
+		score[i] = i * 2;
+	}
+	score[22] = 13;
+	score[23] = 16;
+	score[24] = 19;
+
+	score[25] = 22;
+	score[26] = 24;
+	score[27] = 25;
+
+	score[28] = 28;
+	score[29] = 27;
+	score[30] = 26;
+
+	score[31] = 30;
+	score[32] = 35;
+}
+
 void build_graph() {
 	// 직선 코스
 	for (int i = 0; i <= 20; i++) {
@@ -37,10 +57,6 @@ void build_graph() {
 	g[32] = { 20, 21, 21, 21, 21 };
 }
 
-int find_next_idx(int idx, int move) {
-	return g[idx][move - 1];
-}
-
 int brd[NODE_SIZE];
 int horses[5];
 int ans = -1;
@@ -65,6 +81,8 @@ void dfs(int mi, int score_cur) {
 
 		int prev = horses[hi];
 		int idx_nxt = g[prev][move_cnt[mi] - 1];
+		
+		// SINK는 겹쳐도 됨
 		if (idx_nxt!= SINK && brd[idx_nxt]) continue;
 
 		horses[hi] = idx_nxt;
@@ -84,25 +102,7 @@ int main() {
 		scanf("%d", &move_cnt[i]);
 	}
 
-	// 윷놀이판 점수
-	for (int i = 1; i <= 20; i++) {
-		score[i] = i * 2;
-	}
-	score[22] = 13;
-	score[23] = 16;
-	score[24] = 19;
-
-	score[25] = 22;
-	score[26] = 24;
-	score[27] = 25;
-
-	score[28] = 28;
-	score[29] = 27;
-	score[30] = 26;
-
-	score[31] = 30;
-	score[32] = 35;
-
+	build_score();
 	build_graph();
 	dfs(0, 0);
 	printf("%d", ans);
